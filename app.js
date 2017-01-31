8299a76b2864a5ee17af9b0a457475284d6531f5
Viktors Jenovs <jviktors@gmail.com>
2017-01-31 08:15:16 +0200

    Add socket support

--- app.js
+++ app.js
@@ -5,67 +5,63 @@ console.log('Process:', process.env.NODE_ENV);
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
-// const passport = require('passport');
-// const session = require('express-session');
-// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
 const {
-  PORT,
+  API_KEY,
+  PORT
 } = process.env;
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(MONGODB_URI);
+const data = ['GOOG'];
 
-const app = express();
+const fetchData = (ind) => {
+  const url = `https://www.quandl.com/api/v3/datasets/WIKI/${ind}.json?start_date=2016-01-01&end_date=2017-01-31&api_key=${API_KEY}`
+  return fetch(url)
+}
+
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/ticker/:name', (req, res) => {
-  const url = `https://www.bing.lv/?q=${req.params.name}+ticker+symbol`
-  // const url = `http://www.nasdaq.com/symbol/?Load=true&Search=${req.params.name}`
-  fetch(url)
-    .then(res => res.text())
-    .then(text => {
-      // console.log(text);
-      const result = {};
-      // For bing search
-      const data = /([A-Z]+)(?:<\/strong>)?(?: : Summary for )(?:<strong>)?([A-Za-z\d\s]+)/g.exec(text)
+app.get('/graphs', (req, res) => {
+  console.log('GET /graphs');
+  res.send(data);
+});
 
-      // const re = /(www.nasdaq.com\/symbol\/)(.+)(">)(.+)(<\/a>)/g
-      // const reName = new RegExp(req.params.name, 'gi')
-      // let data;
-      // while (data = re.exec(text)) {
-      //
-      //   console.log('found', data[2], data[4]);
-      //   // re.
-      // }
-
-      // const data = /(www.nasdaq.com\/symbol)(\/.+)(<\/a>)/g.exec(text)
-      // console.log(data.length);
-      // data.forEach(i => console.log(i))
-
-      if (data) {
-        // const ticker = data[0].match(/[A-Z]+/)
-        // console.log(ticker[0]);
-        res.send({
-          name: data[2],
-          ticker: data[1]
-        })
-      } else {
-        res.send({
-          error: 'Nothing found'
-        })
-      }
+app.post('/graphs', (req, res) => {
+  console.log('in POST /graphs', req.body);
+  fetchData(req.body.data)
+    .then(res => res.json())
+    .then(json => {
+      if (json.quandl_error) throw new Error(json.quandl_error);
+      data.push(req.body.data);
+      io.emit('graph updated', (new Date()))
+      res.send(json);
     })
-    .catch(err => console.log(err))
+    .catch(err => res.send(err))
 });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('User connected', socket.id);
+
+  // socket.emit('ping', {hello: 'world'});
+
+  socket.on('test emit', (msg) => {
+    console.log(msg)
+    socket.broadcast.emit('notification')
+  })
+  // io.emit('all', 'message')
+});
+
+server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,72 @@
 import React from 'react';
+import io from 'socket.io-client';
+
+const socket = io();
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      graphs: []
+    }
+    const socket = this.props.socket;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.fetchGraphs();
+    socket.on('graph updated', (data) => {
+      console.log('in graph updated', data);
+      this.fetchGraphs();
+    });
+  }
+
+  fetchGraphs() {
+    fetch('/graphs')
+      .then(res => res.json())
+      .then(graphs => this.setState({graphs}))
+  }
+
+  addGraph(e) {
+    console.log('in addGraph');
+    e.preventDefault();
+    const data = this.refs.symb.value.toUpperCase();
+    if (data) {
+      this.refs.symb.value = '';
+      fetch('/graphs', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({data})
+      })
+      .then(res => res.json())
+      .then(json => console.log(json))
+    }
+  }
+
+  listItems() {
+    return this.state.graphs.map((i, ind) => (
+      <li key={ind}>{i}</li>
+    ));
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <h2>App Component</h2>
+        {this.listItems()}
+        <form>
+          <input
+            ref="symb"
+            type="text"
+            style={{textTransform: "uppercase"}}
+            autoFocus
+          />
+          <button onClick={this.addGraph.bind(this)}>Add</button>
+        </form>
       </div>
     )
   }
